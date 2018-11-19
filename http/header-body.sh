b250aed40b904d7eb7b315694a3d4bb16428ebaa
Björn Bartels <coding@bjoernbartels.earth>
2018-11-19 20:47:31 +0100

    Update http/header-body.sh

--- http/header-body.sh
+++ http/header-body.sh
@@ -19,12 +19,10 @@
 
 http_header_body () {
     
-
-    # (Re)define the specified variable as an associative array.
     unset $1;
     declare -gA $1;
     declare -A response_headers
-    response_body="";
+    body="";
 
     IFS=$'\r';
     
@@ -32,10 +30,6 @@ http_header_body () {
 
     while read -r line
     do
-
-        #echo -e "\nHead: \e[94m$head\e[0m";
-        #echo -e "Line: \e[94m$line\e[0m";
-        #echo -e "Body: \e[94m$body\e[0m";
 
         if [ $head == "true" ]; then 
 
@@ -47,22 +41,11 @@ http_header_body () {
             
                 if [[ "$line" =~ ^HTTP(.*)\ ([0-9]{3})\ (.*)$ ]]; then
 
-                    #echo "--header status line-";
-
                     declare -gA $1["Protocol"]="${BASH_REMATCH[1]}";
                     declare -gA $1["Status"]="${BASH_REMATCH[2]}";
                     declare -gA $1["Statustext"]="${BASH_REMATCH[3]}";
 
-                    #echo -e "Protocol: \e[96m"$$1["Protocol"]"\e[0m";
-                    #echo -e "Status: \e[96m"$$1["Statustext"]"\e[0m";
-                    #echo -e "Statustext: \e[96m"$$1["Statustext"]"\e[0m";
-
                 elif [[ $line =~ ^([[:alnum:]_-]+):\ *(( *[^ ]+)*)\ *$ ]]; then
-
-                    #echo "--header field line-";
-
-                    #echo -e "Field: \e[96m${BASH_REMATCH[1]}\e[0m";
-                    #echo -e "Value: \e[96m${BASH_REMATCH[2]}\e[0m";
 
                     declare -gA $1["${BASH_REMATCH[1]}"]="${BASH_REMATCH[2]}";
 
@@ -72,18 +55,14 @@ http_header_body () {
 
         else
             
-            #echo -e "--body line-";
-            #echo -e "Body line: \e[94m$line\e[0m";
-            
-            response_body="${response_body}"$(echo -e "${line}");
+            body="${body}"$(echo -e "${line/\r//}");
 
         fi
 
     done <<< "$3"
 
-    echo ${response_body};
     unset $2;
-    declare -g $2="${response_body}"
+    declare -g $2="${body}"
     
     unset IFS
 
