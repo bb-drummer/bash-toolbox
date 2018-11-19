52d0107a3e15cc061b6394d9944d6a4fae3094be
Björn Bartels <coding@bjoernbartels.earth>
2018-11-19 17:31:40 +0100

    Update http/header-body.sh

--- http/header-body.sh
+++ http/header-body.sh
@@ -22,10 +22,10 @@ http_header_body () {
     response_message=$3
 
     # (Re)define the specified variable as an associative array.
-    #unset $1;
-    #declare -gA $1;
-    #unset $2;
-    #declare -gA $2;
+    unset $1;
+    declare -gA $1;
+    unset $2;
+    declare -gA $2;
     #local message header body headers
 
     
@@ -58,9 +58,13 @@ http_header_body () {
 
                     echo "--header status line-";
 
-                    response_headers+=(["Protocol"]=${BASH_REMATCH[1]});
-                    response_headers+=(["Status"]=${BASH_REMATCH[2]});
-                    response_headers+=(["Statustext"]=${BASH_REMATCH[3]});
+                    declare -gA $1["Protocol"]="${BASH_REMATCH[1]}";
+                    declare -gA $1["Status"]="${BASH_REMATCH[2]}";
+                    declare -gA $1["Statustext"]="${BASH_REMATCH[3]}";
+
+                    #response_headers+=(["Protocol"]="${BASH_REMATCH[1]})";
+                    #response_headers+=(["Status"]="${BASH_REMATCH[2]})";
+                    #response_headers+=(["Statustext"]="${BASH_REMATCH[3]})";
 
                     echo -e "Protocol: \e[96m${response_headers[Protocol]}\e[0m";
                     echo -e "Status: \e[96m${response_headers[Status]}\e[0m";
@@ -73,7 +77,9 @@ http_header_body () {
                     echo -e "Line: \e[96m${BASH_REMATCH[0]}\e[0m";
                     echo -e "Field: \e[96m${BASH_REMATCH[1]}\e[0m";
                     echo -e "Value: \e[96m${BASH_REMATCH[2]}\e[0m";
-                    response_headers+=(["${BASH_REMATCH[1]}"]=${BASH_REMATCH[2]});
+
+                    declare -gA $1["${BASH_REMATCH[1]}"]="${BASH_REMATCH[2]}";
+                    #response_headers+=(["${BASH_REMATCH[1]}"]="${BASH_REMATCH[2]})";
 
                 fi
 
@@ -89,8 +95,8 @@ http_header_body () {
 
     done <<< "$3"
 
-    declare -gA $1=${response_headers}
-    declare -g $2=${response_body}
+    #declare -gA $1=${response_headers}
+    declare -g $2="${response_body}""
     
     unset IFS
 
