bec09a210c86cd60810969ba1e3dc0ef76a7d17e
Björn Bartels <coding@bjoernbartels.earth>
2018-11-19 15:02:44 +0100

    Update http/header-body.sh

--- http/header-body.sh
+++ http/header-body.sh
@@ -19,6 +19,8 @@
 
 http_header_body () {
     
+    response=echo "$3"
+
     # (Re)define the specified variable as an associative array.
     unset $1;
     declare -gA $1;
@@ -26,12 +28,70 @@ http_header_body () {
     declare -gA $2;
     #local message header body headers
 
+    
     declare -A response_headers=();
     declare response_body="";
 
 
 
-    head=true
+    response=(${response[@]}) # convert to array
+
+    for idx in ${response}; do
+
+        line=${response[$idx]};
+        echo -e "Line: \e[94m$line\e[0m";
+
+
+        if $head; then 
+            if [[ $line = $'\r' ]]; then
+                head=false
+            else
+                HTTP_line='^(.*) ([0-9]{3}) (.*)$';
+                #Field_line='^(.*): (.*)$';
+                #Field_line='(.*):(.*)';
+
+                #header="$header"$'\n'"$line"
+                #if [[ "$line" =~ $HTTP_line ]]; then
+                if [[ "$line" =~ ^(.*)\ ([0-9]{3})\ (.*)$ ]]; then
+
+                    echo "--header status line-";
+                    response_headers+=(["Protocol"]=${BASH_REMATCH[1]});
+                    response_headers+=(["Status"]=${BASH_REMATCH[2]});
+                    response_headers+=(["Statustext"]=${BASH_REMATCH[3]});
+
+                elif [[ $line =~ ^([[:alnum:]_-]+):\ *(( *[^ ]+)*)\ *$ ]]; then
+
+
+                    echo "--header field line-";
+
+                    echo -e "Line: \e[96m${BASH_REMATCH[0]}\e[0m";
+                    echo -e "Field: \e[96m${BASH_REMATCH[1]}\e[0m";
+                    echo -e "Value: \e[96m${BASH_REMATCH[2]}\e[0m";
+                    response_headers+=(["${BASH_REMATCH[1]}"]=${BASH_REMATCH[2]});
+
+                fi
+            fi
+        else
+
+            response_body=echo "${response_body}\n${line}";
+
+        fi
+
+    done
+
+    declare -gA $1=${response_headers}
+    declare -g $2=${response_body}
+    
+
+<< ////
+
+    code=${response[-1]} # get last element (last line)
+    body=${response[@]::${#response[@]}-1} # get all elements except last
+    name=$(echo $body | jq '.name')
+    echo $code
+    echo "name: "$name
+
+   head=true
     headercount=0
     while read -r line; do 
 
@@ -77,7 +137,6 @@ http_header_body () {
     declare -gA $1=${response_headers}
     declare -g $2=${response_body}
 
-<< ////
     #done < <(printf "%s" "$3")
     #elif [[ "$line" =~ $Field_line ]]; then
 ////
