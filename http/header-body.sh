be8fa94c23ce5cf9e646203699398ce743278962
Björn Bartels <coding@bjoernbartels.earth>
2018-11-19 14:11:16 +0100

    Update http/header-body.sh

--- http/header-body.sh
+++ http/header-body.sh
@@ -26,7 +26,7 @@ http_header_body () {
     declare -gA $2;
     #local message header body headers
 
-    declare -A response_headers;
+    declare -A response_headers=();
 
 
 
@@ -38,18 +38,19 @@ http_header_body () {
                 head=false
             else
                 #header="$header"$'\n'"$line"
-                if [[ $line =~ "^(.*) ([0-9]{3}) (.*)$" ]]; then
+                if [[ "$line" =~ "^(.*) ([0-9]{3}) (.*)$" ]]; then
+                    echo "--header status line-";
+                    response_headers+=([Protocol]=${BASH_REMATCH[1]});
+                    response_headers+=([Status]=${BASH_REMATCH[2]});
+                    response_headers+=([Statustext]=${BASH_REMATCH[3]});
 
-                    response_headers[Protocol]=${BASH_REMATCH[1]}
-                    response_headers[Status]=${BASH_REMATCH[2]}
-                    response_headers[Statustext]=${BASH_REMATCH[3]}
-
-                elif [[ $line =~ "^(.*): (.*)$" ]]; then
+                elif [[ "$line" =~ "^(.*): (.*)$" ]]; then
+                    echo "--header field line-";
 
                     echo -e "Line: \e[96m${BASH_REMATCH[0]}\e[0m";
                     echo -e "Field: \e[96m${BASH_REMATCH[1]}\e[0m";
                     echo -e "Value: \e[96m${BASH_REMATCH[2]}\e[0m";
-                    response_headers+=([${BASH_REMATCH[1]}]=${BASH_REMATCH[2]})
+                    response_headers+=([${BASH_REMATCH[1]}]=${BASH_REMATCH[2]});
 
                 else
 
