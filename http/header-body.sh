589e7262d49d7dfdc732333dd3f6aa48470f6852
Björn Bartels <coding@bjoernbartels.earth>
2018-11-19 16:26:32 +0100

    Update http/header-body.sh

--- http/header-body.sh
+++ http/header-body.sh
@@ -38,7 +38,8 @@ http_header_body () {
     #response=(${response[@]}) # convert to array
     
     #for line in ${response}; do
-    while read -d '' -r line
+
+    while read -r line
     do
 
         #printf "%s" "Line: \e[94m$line\e[0m";
@@ -46,7 +47,6 @@ http_header_body () {
 
         echo -e "Line: \e[94m$line\e[0m";
 
-<< ////
         if $head; then 
             if [[ $line = $'\r' ]]; then
                 head=false
@@ -80,7 +80,6 @@ http_header_body () {
             response_body=echo "${response_body}\n${line}";
 
         fi
-////
 
     done <<< "$3"
 
@@ -89,64 +88,6 @@ http_header_body () {
     
     unset IFS
 
-
-<< ////
-
-    code=${response[-1]} # get last element (last line)
-    body=${response[@]::${#response[@]}-1} # get all elements except last
-    name=$(echo $body | jq '.name')
-    echo $code
-    echo "name: "$name
-
-   head=true
-    headercount=0
-    while read -r line; do 
-
-
-        echo -e "Line: \e[94m$line\e[0m";
-
-
-        if $head; then 
-            if [[ $line = $'\r' ]]; then
-                head=false
-            else
-                HTTP_line='^(.*) ([0-9]{3}) (.*)$';
-                #Field_line='^(.*): (.*)$';
-                #Field_line='(.*):(.*)';
-
-                #header="$header"$'\n'"$line"
-                #if [[ "$line" =~ $HTTP_line ]]; then
-                if [[ "$line" =~ ^(.*)\ ([0-9]{3})\ (.*)$ ]]; then
-
-                    echo "--header status line-";
-                    response_headers+=(["Protocol"]=${BASH_REMATCH[1]});
-                    response_headers+=(["Status"]=${BASH_REMATCH[2]});
-                    response_headers+=(["Statustext"]=${BASH_REMATCH[3]});
-
-                elif [[ $line =~ ^([[:alnum:]_-]+):\ *(( *[^ ]+)*)\ *$ ]]; then
-
-
-                    echo "--header field line-";
-
-                    echo -e "Line: \e[96m${BASH_REMATCH[0]}\e[0m";
-                    echo -e "Field: \e[96m${BASH_REMATCH[1]}\e[0m";
-                    echo -e "Value: \e[96m${BASH_REMATCH[2]}\e[0m";
-                    response_headers+=(["${BASH_REMATCH[1]}"]=${BASH_REMATCH[2]});
-
-                fi
-            fi
-        else
-            response_body="$response_body"$'\n'"$line"
-        fi
-
-    done <(echo "$3")
-
-    declare -gA $1=${response_headers}
-    declare -g $2=${response_body}
-
-    #done < <(printf "%s" "$3")
-    #elif [[ "$line" =~ $Field_line ]]; then
-////
 
 }
 
