87e271d08da72aee16edc9e0b6dd955306bd7072
Björn Bartels <coding@bjoernbartels.earth>
2018-11-19 14:16:16 +0100

    Update http/header-body.sh

--- http/header-body.sh
+++ http/header-body.sh
@@ -37,20 +37,23 @@ http_header_body () {
             if [[ $line = $'\r' ]]; then
                 head=false
             else
-                #header="$header"$'\n'"$line"
-                if [[ "$line" =~ "^(.*) ([0-9]{3}) (.*)$" ]]; then
-                    echo "--header status line-";
-                    response_headers+=([Protocol]=${BASH_REMATCH[1]});
-                    response_headers+=([Status]=${BASH_REMATCH[2]});
-                    response_headers+=([Statustext]=${BASH_REMATCH[3]});
+                HTTP_line='^(.*) ([0-9]{3}) (.*)$';
+                Field_line='^(.*): (.*)$';
 
-                elif [[ "$line" =~ "^(.*): (.*)$" ]]; then
+                #header="$header"$'\n'"$line"
+                if [[ "$line" =~ $HTTP_line ]]; then
+                    echo "--header status line-";
+                    response_headers+=(["Protocol"]=${BASH_REMATCH[1]});
+                    response_headers+=(["Status"]=${BASH_REMATCH[2]});
+                    response_headers+=(["Statustext"]=${BASH_REMATCH[3]});
+
+                elif [[ "$line" =~ $Field_line ]]; then
                     echo "--header field line-";
 
                     echo -e "Line: \e[96m${BASH_REMATCH[0]}\e[0m";
                     echo -e "Field: \e[96m${BASH_REMATCH[1]}\e[0m";
                     echo -e "Value: \e[96m${BASH_REMATCH[2]}\e[0m";
-                    response_headers+=([${BASH_REMATCH[1]}]=${BASH_REMATCH[2]});
+                    response_headers+=(["${BASH_REMATCH[1]}"]=${BASH_REMATCH[2]});
 
                 else
 
